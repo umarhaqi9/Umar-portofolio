4c865cab595fcdeab252225dd71e86d0d1cb9a81
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-08-02 13:31:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import './App.css';
 import React, { useEffect, useState, useRef } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button , Nav, Container, Navbar, Row, Col, Carousel} from 'react-bootstrap';
-import AOS from 'aos';
 import "aos/dist/aos.css";
-import emailjs from '@emailjs/browser';
-import { gsap } from "gsap";
-import { TextPlugin } from "gsap/TextPlugin";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import VanillaTilt from 'vanilla-tilt';
 import Home from './Pages/Home';
 import Personal from './Pages/Personal';
 
